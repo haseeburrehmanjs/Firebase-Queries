4f7dab36af093c6a3a9ecd3b87ea77507ab08d8b
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-08-16 23:11:06 +0500

    again practice todo app

--- main.js
+++ main.js
@@ -34,93 +34,169 @@ let input = document.querySelector("#todo")
 let select = document.querySelector("#select")
 let filterBtn = document.querySelectorAll(".filterBtn")
 
-filterBtn.forEach((btn, index) => {
-    btn.addEventListener('click', async () => {
-        addTodo= []
-        list.innerHTML = ''
-        const queries = collection(db, "todos");
-        const q = query(queries, where("cities", "==", btn.innerHTML));
-        const querySnapshot = await getDocs(q);
-        querySnapshot.forEach((doc) => {
-            addTodo.push({ ...doc.data(), id: doc.id });
-        });
-        console.log(addTodo);
-        renderScreen()
-    })
-})
+// empty array for push data 
+let emptyArray = []
 
-// add todo in array
-let addTodo = []
-
-// get data from firestore database
-async function getDataFromDb() {
-    const querySnapshot = await getDocs(collection(db, "todos"));
-    querySnapshot.forEach((doc) => {
-        addTodo.push({ ...doc.data(), id: doc.id })
-    });
-    renderScreen()
-}
-getDataFromDb()
-
-// function for render screen
-function renderScreen() {
-    list.innerHTML = ''
-    addTodo.map((item, index) => {
-        list.innerHTML += `
-        <li>${item.todos} - ${item.createdAt}
-        <button class="deleteBtn">delete</button>
-        <button class="editBtn">edit</button>
-        </li>`
-
-        let deleteBtn = document.querySelectorAll('.deleteBtn')
-        let editBtn = document.querySelectorAll('.editBtn')
-
-        deleteBtn.forEach((btn, index) => {
-            btn.addEventListener('click', async () => {
-                await deleteDoc(doc(db, "todos", addTodo[index].id));
-                addTodo.splice(index, 1)
-                renderScreen()
-            })
-        })
-
-        editBtn.forEach((btn, index) => {
-            btn.addEventListener('click', async () => {
-                let ubdateValue = prompt('enter todo')
-                const ubpdateDoc = doc(db, "todos", addTodo[index].id);
-                await updateDoc(ubpdateDoc, {
-                    todos: ubdateValue
-                });
-                addTodo[index].todos = ubdateValue
-                renderScreen()
-            })
-        })
+// for render screen 
+function renderScreen(){
+    emptyArray.map((item, index) => {
+        console.log(item);
     })
 }
 renderScreen()
 
-// input value push in empty array
-form.addEventListener("submit", async event => {
+// get input value and store firestore database
+form.addEventListener('submit', event => {
     event.preventDefault()
-    if (input.value === '') {
-        alert
-    } else {
-        try {
-            const docRef = await addDoc(collection(db, "todos"), {
-                todos: input.value,
-                createdAt: new Date().toISOString(),
-                cities: select.value,
-            });
-            addTodo.push({
-                todos: input.value,
-                createdAt: new Date().toISOString(),
-                cities: select.value,
-                id: docRef.id
-            })
-            console.log("Document written with ID: ", docRef.id);
-        } catch (e) {
-            console.error("Error adding document: ", e);
-        }
+    
+    if(input.value === '') {
+        alert('please fill input')
+    }else {
+        
+
+        emptyArray.push({
+            todo: input.value,
+            createdAt: new Date().toISOString(),
+            city: select.value
+        })
+        renderScreen()
     }
-    renderScreen()
-    input.value = ''
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// filterBtn.forEach((btn, index) => {
+//     btn.addEventListener('click', async () => {
+//         addTodo= []
+//         list.innerHTML = ''
+//         const queries = collection(db, "todos");
+//         const q = query(queries, where("cities", "==", btn.innerHTML));
+//         const querySnapshot = await getDocs(q);
+//         querySnapshot.forEach((doc) => {
+//             addTodo.push({ ...doc.data(), id: doc.id });
+//         });
+//         console.log(addTodo);
+//         renderScreen()
+//     })
+// })
+
+// // add todo in array
+// let addTodo = []
+
+// // get data from firestore database
+// async function getDataFromDb() {
+//     const querySnapshot = await getDocs(collection(db, "todos"));
+//     querySnapshot.forEach((doc) => {
+//         addTodo.push({ ...doc.data(), id: doc.id })
+//     });
+//     renderScreen()
+// }
+// getDataFromDb()
+
+// // function for render screen
+// function renderScreen() {
+//     list.innerHTML = ''
+//     addTodo.map((item, index) => {
+//         list.innerHTML += `
+//         <li>${item.todos} - ${item.createdAt}
+//         <button class="deleteBtn">delete</button>
+//         <button class="editBtn">edit</button>
+//         </li>`
+
+//         let deleteBtn = document.querySelectorAll('.deleteBtn')
+//         let editBtn = document.querySelectorAll('.editBtn')
+
+//         deleteBtn.forEach((btn, index) => {
+//             btn.addEventListener('click', async () => {
+//                 await deleteDoc(doc(db, "todos", addTodo[index].id));
+//                 addTodo.splice(index, 1)
+//                 renderScreen()
+//             })
+//         })
+
+//         editBtn.forEach((btn, index) => {
+//             btn.addEventListener('click', async () => {
+//                 let ubdateValue = prompt('enter todo')
+//                 const ubpdateDoc = doc(db, "todos", addTodo[index].id);
+//                 await updateDoc(ubpdateDoc, {
+//                     todos: ubdateValue
+//                 });
+//                 addTodo[index].todos = ubdateValue
+//                 renderScreen()
+//             })
+//         })
+//     })
+// }
+// renderScreen()
+
+// // input value push in empty array
+// form.addEventListener("submit", async event => {
+//     event.preventDefault()
+//     if (input.value === '') {
+//         alert
+//     } else {
+//         try {
+//             const docRef = await addDoc(collection(db, "todos"), {
+//                 todos: input.value,
+//                 createdAt: new Date().toISOString(),
+//                 cities: select.value,
+//             });
+//             addTodo.push({
+//                 todos: input.value,
+//                 createdAt: new Date().toISOString(),
+//                 cities: select.value,
+//                 id: docRef.id
+//             })
+//             console.log("Document written with ID: ", docRef.id);
+//         } catch (e) {
+//             console.error("Error adding document: ", e);
+//         }
+//     }
+//     renderScreen()
+//     input.value = ''
+// })
+
